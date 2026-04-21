588e5e730b8df331ed47c43bf6dd4d8a987e422a
UnknownTehk <brendanjhnsn@outlook.com>
2026-04-21 19:24:19 -0400

    Updated

--- test.js
+++
@@ -1,5 +0,0 @@
-const greetUserArrow = (name, timeOfDay) => { // name and timeOfDay are parameters that will be passed in when the function is called
-    console.log(`Good ${timeOfDay}, ${name}!`); // this will print "Good timeOfDay, name!" to the console
-}
-
-greetUserArrow("Brendan", "Morning"); // this will print "Good Morning, Brendan!" to the console
